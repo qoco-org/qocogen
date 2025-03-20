a725d1a50e4d5bcdb54124a437e7e9d7802f5e30
govindchari <govindchari1@gmail.com>
2025-03-20 15:51:54 -0700

    Dont link with m in msvc

--- qocogen/codegen.py
+++ qocogen/codegen.py
@@ -167,7 +167,9 @@ def generate_cmakelists(solver_dir):
     f.write(
         "target_sources(qoco_custom PRIVATE ${qoco_custom_sources} ${qoco_custom_headers})\n"
     )
-    f.write("target_link_libraries(qoco_custom m)\n\n")
+    f.write('if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux" OR ${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")')
+    f.write("   target_link_libraries(qoco_custom m)\n")
+    f.write("endif()\n\n")
     f.write("# Build qoco demo.\n")
     f.write("add_executable(runtest runtest.c)\n")
     f.write("target_link_libraries(runtest qoco_custom)\n")
